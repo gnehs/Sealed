a07e16dbc590494c183a52e7634fa4a8ad9230dc
gnehs <jayuiop@gmail.com>
2018-11-10 12:05:19 +0800

    Revert "嘗試採用 translate"
    This reverts commit c76a8d543edb1a5fe927b9110154b2ccc6469be5.

--- negi/negi.js
+++ negi/negi.js
@@ -1,19 +1,17 @@
-(function (f, a, d, h) {
-    var g = (function () {
+(function(f, a, d, h) {
+    var g = (function() {
         var i = f.getComputedStyle,
             k, j, l = '';
         if (i) {
             k = i(a.documentElement, '');
             k = Array.prototype.join.call(k, '');
             j = k.match(/-(?:O|Moz|webkit|ms)-/i);
-            if (j) {
-                l = j[0]
-            }
+            if (j) { l = j[0] }
         }
         return l
     })();
     var c = {};
-    (function (i) {
+    (function(i) {
         var n = (f.requestAnimationFrame || f.mozRequestAnimationFrame || f.webkitRequestAnimationFrame || f.msRequestAnimationFrame || setTimeout).bind(f);
         var m = (f.cancelAnimationFrame || f.mozcancelAnimationFrame || f.webkitcancelAnimationFrame || f.mscancelAnimationFrame || clearTimeout).bind(f);
         var k = {};
@@ -24,9 +22,7 @@
             function s(v) {
                 if (k[u].playing) {
                     t(v + k[u].iShift);
-                    if (!k[u]) {
-                        return
-                    }
+                    if (!k[u]) { return }
                     k[u].frameId = n(s);
                     k[u].lastTime = v
                 }
@@ -38,34 +34,21 @@
                 u;
             s = s || 0;
             l++;
-            k[t] = {
-                frameId: 0,
-                playing: true,
-                thread: u,
-                lastTime: 0,
-                shift: s,
-                iShift: s - (Date.now() - o)
-            };
+            k[t] = { frameId: 0, playing: true, thread: u, lastTime: 0, shift: s, iShift: s - (Date.now() - o) };
             u = j(t, u);
             n(u);
             return t
         };
         i.clearRender = function r(s) {
             if (k[s] !== h) {
-                if (k[s].playing) {
-                    m(k[s].frameId)
-                }
+                if (k[s].playing) { m(k[s].frameId) }
                 k[s] = h;
                 return true
-            } else {
-                return false
-            }
+            } else { return false }
         };
         i._init = function p() {
             function t() {
-                function u(v) {
-                    o = Date.now() - v
-                }
+                function u(v) { o = Date.now() - v }
                 return u
             }
             var s = t();
@@ -79,14 +62,8 @@
     if (b === h) {
         b = a.getElementsByTagName('head')[0];
         b.appendChild(e)
-    } else {
-        if (b.parentNode.lastChild == b) {
-            b.parentNode.appendChild(e)
-        } else {
-            b.parentNode.insertBefore(e, b.nextSibling)
-        }
-    }
-    f.Negi = (function () {
+    } else { if (b.parentNode.lastChild == b) { b.parentNode.appendChild(e) } else { b.parentNode.insertBefore(e, b.nextSibling) } }
+    f.Negi = (function() {
         function i() {
             var m = a.documentElement,
                 k = a.body,
@@ -94,82 +71,63 @@
                 j = f.innerHeight || m.clientHeight || k.clientHeight;
             this.__instance = a.createElement('img');
             this.__instance.setAttribute('class', 'negi');
-            this.setInlineCSS(this.__instance, {
-                left: Math.random() * l + 'px',
-                top: '-10px'
-            });
+            this.setInlineCSS(this.__instance, { left: Math.random() * l + 'px', top: '-10px' });
             this.__speed = 0;
             this.__max_height = j + 30;
             this.__run_times = 0;
             this.__render_id = -1;
             this.__fibo = [2, 3, 5, 8, 13, 21, 34, 55, 89]
         }
-        i.prototype.start = function () {
+        i.prototype.start = function() {
             this.__instance.src = 'https://gnehs.github.io/Sealed/negi/img/negi' + (Math.ceil(Math.random() * 6)) + '.png';
             a.getElementsByTagName('body')[0].appendChild(this.__instance);
             var k = this,
-                j = function (_css_rotateZ) {
+                j = function(m) {
                     var p, o, t, s = 0,
                         r = 0,
-                        _css_scale, n;
+                        q, n;
                     k.__run_times++;
-                    for (o = 0, t = k.__fibo.length; o < t; o++) {
-                        p = k.__fibo[o];
-                        if (k.__run_times % p === 0) {
-                            k.__speed += Math.random() - 0.5
-                        }
-                    }
+                    for (o = 0, t = k.__fibo.length; o < t; o++) { p = k.__fibo[o]; if (k.__run_times % p === 0) { k.__speed += Math.random() - 0.5 } }
                     n = k.getInlineCSS(k.__instance);
-                    if (n.hasOwnProperty('left')) {
-                        s = parseFloat(n.left)
-                    }
+                    if (n.hasOwnProperty('left')) { s = parseFloat(n.left) }
                     if (k.__run_times > 200 || r >= k.__max_height) {
                         a.getElementsByTagName('body')[0].removeChild(k.__instance);
                         c.clearRender(k.__intervalID);
                         return false
                     }
                     s += k.__speed > 0 ? Math.abs(k.__speed) : Math.abs(k.__speed) * -1;
-                    r = _css_rotateZ * 0.25;
-                    _css_scale = 1 + _css_rotateZ * 0.001;
-                    _css_scale = _css_scale > 2 ? 2 : _css_scale;
+                    r = m * 0.25;
+                    q = 1 + m * 0.001;
+                    q = q > 2 ? 2 : q;
                     var l = {};
-                    l.transform = `rotateZ(${_css_rotateZ}deg) scale(${_css_scale}) translate(${s}px,${r}px)`;
+                    l[g + 'transform'] = 'rotateZ(' + m + 'deg) scale(' + q + ')';
+                    l.transform = 'rotateZ(' + m + 'deg) scale(' + q + ')';
+                    l.left = s + 'px';
+                    l.top = r + 'px';
                     k.setInlineCSS(k.__instance, l)
                 };
             this.__intervalID = c.setRender(j)
         };
-        i.prototype.getInlineCSS = function (j) {
-            if (j.__style) {
-                return j.__style
-            }
+        i.prototype.getInlineCSS = function(j) {
+            if (j.__style) { return j.__style }
             var k = j.getAttribute('style') || '';
             var m = /([^:\s]+)\s*:\s*([^;]+)/g;
             var l = {};
-            k.replace(m, function (n, o, p) {
-                l[o] = p.trim()
-            });
+            k.replace(m, function(n, o, p) { l[o] = p.trim() });
             j.__style = l;
             return l
         };
-        i.prototype.setInlineCSS = function (l, m) {
+        i.prototype.setInlineCSS = function(l, m) {
             var j = this.getInlineCSS(l),
                 n = [],
                 k;
-            for (k in m) {
-                if (m.hasOwnProperty(k)) {
-                    j[k] = m[k]
-                }
-            }
-            for (k in j) {
-                if (j.hasOwnProperty(k)) {
-                    n.push(k + ': ' + j[k])
-                }
-            }
+            for (k in m) { if (m.hasOwnProperty(k)) { j[k] = m[k] } }
+            for (k in j) { if (j.hasOwnProperty(k)) { n.push(k + ': ' + j[k]) } }
             l.setAttribute('style', n.join('; '))
         };
         return i
     })();
-    setInterval(function () {
+    setInterval(function() {
         (new Negi()).start()
     }, 500)
 })(this, this.document, 'style')
